586682d0a19b6996715042369101a77fb2a3a9ed
yu93652510 <47739156+yu93652510@users.noreply.github.com>
2019-02-18 20:29:50 +0800

    Update answer1.cs

--- answer1.cs
+++ answer1.cs
@@ -5,7 +5,7 @@ public class Program
 	public static void Main()
 	{
 		string[] array1 = { "A", "C","D","A"};
-		string[] array2 = {"C","D","X"};
+		string[] array2 = {"C","X","C"};
 		if (isSubset(array1,array2)){
 			Console.WriteLine("true");
 		}else{
@@ -15,19 +15,14 @@ public class Program
 	
 	static bool isSubset(string[] arr1, string[] arr2){
 		int arr1Length = arr1.Length;
-		Console.WriteLine(arr1Length);
 		int arr2Length = arr2.Length;
-		Console.WriteLine(arr2Length);
 		
 		for (int i = 0; i < arr2Length ; i ++){
-			 Console.WriteLine("i=" + i);
 			for (int j = 0; j < arr1Length ; j++){
-				Console.WriteLine("j=" + j);
 				if(arr1[j].Equals(arr2[i])==true){
 					break;
 				}
 				if( j == arr1Length -1){
-					Console.WriteLine("hi");
 					return false;
 				}
 			}
@@ -35,8 +30,3 @@ public class Program
 		}		
 		return true;
 	}
-
-
-
-
-}
